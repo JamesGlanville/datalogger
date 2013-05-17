424cd2313582a54335ae0cbee3b80a1c3312a6fd
James Glanville <james.n.glanville@gmail.com>
2013-05-17 17:05:30 +0100

    gah lots

--- 3DAQ/Firmware/main.d
+++ 3DAQ/Firmware/main.d
@@ -31,3 +31,5 @@ main.o: .\lib\stm32f10x_stdperiph_driver\inc\misc.h
 main.o: .\user\inc\LEDs.h
 main.o: .\user\inc\UART.h
 main.o: .\user\inc\ADC.h
+main.o: user\src\HD44780.h
+main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
--- 3DAQ/Firmware/user/src/LEDs.c
+++ 3DAQ/Firmware/user/src/LEDs.c
@@ -21,6 +21,34 @@ void LED_init()
   GPIO_Init(GPIOC, &GPIO_InitStructure);	//Call the GPIO init function from the std periph lib to initialise GPIOC with these parameters
 }
 
+void LCD_init()
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;	//Structure that holds configuration values for GPIO (General Purpose In/Out) pins
+	
+	//start GPIOC clock to enable GPIO Port C
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	//Configure Port C Pin 9 (PC9) to drive LED (ie configure as output)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2;	//Choose pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;	//Choose mode (output, push-pull)
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;	//Choose max speed (slow is fine here)
+  GPIO_Init(GPIOB, &GPIO_InitStructure);	//Call the GPIO init function from the std periph lib to initialise GPIOC with these parameters
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5;	//Choose pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;	//Choose mode (output, push-pull)
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;	//Choose max speed (slow is fine here)
+  GPIO_Init(GPIOC, &GPIO_InitStructure);	//Call the GPIO init function from the std periph lib to initialise GPIOC with these parameters
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;	//Choose pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;	//Choose mode (output, push-pull)
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;	//Choose max speed (slow is fine here)
+  GPIO_Init(GPIOA, &GPIO_InitStructure);	//Call the GPIO init function from the std periph lib to initialise GPIOC with these parameters
+	
+	
+}
+
 //Function to turn LED off
 void LED_off()
 {
