98c4393fd2f8a539b131b892ff2158abf04113b6
Andy Holt <andrew.holt@hotmail.co.uk>
2013-05-23 09:39:37 +0100

    menu and stuff

--- gui/datalogger.cpp
+++ gui/datalogger.cpp
@@ -33,18 +33,17 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const
   : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
   wxMenu *menuFile = new wxMenu;
-  
   menuFile->Append(wxID_EXIT);
-  
   wxMenu *menuData = new wxMenu;
-
-  menuData->Append(ID_StartLog, wxT("&Start Logging\tCtrl-L"), wxT("Help string: Begin logging with board plugged in."));
-  menuData->Append(ID_StopLog, wxT("&Stop Logging\tCtrl-K"), wxT("Help string: Stop logging with board plugged in."));
-  menuData->Append(ID_GetData, wxT("&Get Data\tCtrl-D"), wxT("Help string: Get data from board."));
-  menuData->Append(ID_EraseData, wxT("&Erase Data\tCtrl-E"), wxT("Help string: Erase chip memory."));
-
+  menuData->Append(ID_StartLog, wxT("&Start Logging\tCtrl-L"),
+		   wxT("Help string: Begin logging with board plugged in."));
+  menuData->Append(ID_StopLog, wxT("&Stop Logging\tCtrl-K"),
+		   wxT("Help string: Stop logging with board plugged in."));
+  menuData->Append(ID_GetData, wxT("&Get Data\tCtrl-D"),
+		   wxT("Help string: Get data from board."));
+  menuData->Append(ID_EraseData, wxT("&Erase Data\tCtrl-E"),
+		   wxT("Help string: Erase chip memory."));
   wxMenu *menuHelp = new wxMenu;
-
   menuHelp->Append(wxID_ABOUT);
   
   wxMenuBar *menuBar = new wxMenuBar;
@@ -67,7 +66,9 @@ void MyFrame::OnExit(wxCommandEvent& event)
 
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
-  wxMessageBox(wxT("This is the interface for Parcel Tracker.\n By James Glanville and Andy Holt.\n Created using wxWidgets."),
+  wxMessageBox(wxT("This is the interface for Parcel Tracker.\n\
+                    By James Glanville and Andy Holt.\n\
+                    Created using wxWidgets."),
 	       wxT("About Parcel Tracker"), wxOK | wxICON_INFORMATION);
 }
 
